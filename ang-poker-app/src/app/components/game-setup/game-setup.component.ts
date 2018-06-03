2291ed1d85952a2592b26996aee33afef1403e80
JJ Mubang <jmubang@gmail.com>
2018-06-03 17:49:30 -0400

    getting filtering setup

--- ang-poker-app/src/app/components/game-setup/game-setup.component.ts
+++ ang-poker-app/src/app/components/game-setup/game-setup.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-setup.component.css']
 })
 export class GameSetupComponent implements OnInit {
+  public filteredUsers: any;
+  public users: any;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+assignCopy() {
+  this.filteredUsers = Object.assign([], this.users);
+}
+/* filterItem(value){ */
+//   if(!value) { this.assignCopy() }
+//   this.filteredItems = Object.assign([], this.items).filter(
+//       item => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+//    )
+// }
+/* this.assignCopy(); */
 }
