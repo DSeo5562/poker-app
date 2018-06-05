c2e83d22d419c756bb1bb566671fac190fd96658
Kevin Magno <kmagno94@gmail.com>
2018-06-04 22:37:15 -0400

    Changed config to AWS DB and added controllers

---
+++ PokerApp/src/main/java/com/revature/controller/CurrentHandsController.java
@@ -0,0 +1,29 @@
+package com.revature.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.revature.beans.CurrentHands;
+import com.revature.service.CurrentHandsService;
+
+@Controller("currentHandsController")
+@RequestMapping("/currentHands")
+public class CurrentHandsController {
+
+	@Autowired
+	private CurrentHandsService currentHandsService;
+
+	@GetMapping("/all")
+	@ResponseBody
+	public ResponseEntity<List<CurrentHands>> getAllUsers() {
+		return new ResponseEntity<>(currentHandsService.getCurrentHands(), HttpStatus.OK);
+	}
+	
+}
